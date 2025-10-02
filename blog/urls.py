d0725e9cf00de06f8f87f7d2a3ed03676368e92c
armanarstambekov <armanarstambekov@gmail.com>
2025-10-02 09:51:20 +0500

    blo.

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/', views.post_list, name='post_list_by_path'),  # сюда добавил
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('post/', views.post_list, name='post_list_by_path'),
 ]
